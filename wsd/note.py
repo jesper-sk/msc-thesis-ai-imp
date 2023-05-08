dccce0708ec52d838468c0518207970d79eefde8
jesper-sk <47450353+JesperK123456@users.noreply.github.com>
2023-05-08 20:46:49 +0200

    Trials with transformers

--- wsd/note.py
+++ wsd/note.py
@@ -1,4 +1,30 @@
 # %%
-from coarsewsd20 import *
+import coarsewsd20 as cwsd
+# %%
+import torch
+from transformers import BertConfig, BertModel, BertTokenizer
 
 # %%
+config = BertConfig.from_pretrained("bert-base-uncased")
+tokenizer = BertTokenizer.from_pretrained("bert-base-uncased")
+model = BertModel.from_pretrained("bert-base-uncased", config=config)
+
+# %%
+
+model.eval()
+
+# %%
+texts = [
+    "Hello, my dog is cute",
+    "Hi, their cat is very cute",
+]
+
+pad_encoding = tokenizer.encode(
+    texts, padding=True, truncation=True, max_length=512)
+
+# TODO: CWSD20 is already tokenized, but needs to be encoded
+# Already includes [CLS] and [SEP]
+tokens = [tokenizer.encode(text) for text in texts]
+# tokens = torch.tensor([tokens]) # Create tensor to for input to model
+# %%
+1
