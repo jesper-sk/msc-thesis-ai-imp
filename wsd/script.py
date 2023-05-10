ddc6831ecf1f7cde1b2ac3b0d0bd67bf2577d905
jesper-sk <47450353+JesperK123456@users.noreply.github.com>
2023-05-10 19:37:21 +0200

    Vectorisation

--- wsd/script.py
+++ wsd/script.py
@@ -1,13 +1,18 @@
 # %%
 import coarsewsd20 as cwsd
-from encode import encode
-
-data = cwsd.load_dataset(cwsd.Variant.REGULAR)
+from transformers import BertModel, BertTokenizerFast
+from vectorise import *
 
 # %%
-from transformers import AutoTokenizer
 
-tokenizer = AutoTokenizer.from_pretrained("bert-base-uncased")
+data = cwsd.load_dataset(cwsd.Variant.REGULAR)
+tokenizer = BertTokenizerFast.from_pretrained("bert-base-uncased")
+encoder = encode(tokenizer, data, "train")
+
+model = BertModel.from_pretrained("bert-base-uncased")
+vectoriser = vectorise(encoder, model)
+
+
 # %%
 foo = [x for x in encode(tokenizer, data, "train")]
 # %%
--- wsd/vectorise.py
+++ wsd/vectorise.py
@@ -7,36 +7,42 @@ from transformers.tokenization_utils import BatchEncoding
 from transformers.utils.generic import PaddingStrategy, TensorType
 
 CoarseWSD20DataSet = dict[str, cwsd.WordDataset]
-Tokenizer = trans.PreTrainedTokenizerFast | trans.PreTrainedTokenizer
+Tokenizer = trans.BertTokenizerFast | trans.BertTokenizerFast
+Model = trans.BertModel
+Encoder = Iterator[tuple[str, BatchEncoding]]
 
 
 def encode(
     tokenizer: Tokenizer, data: CoarseWSD20DataSet, split: cwsd.Split
-) -> Iterator[tuple[str, BatchEncoding]]:
-    for word in data.keys():
-        logging.info("Processing word '%s' in %s...", word, split)
-        word_tokens = [
-            tokens
-            for tokens in data[word].tokens(split)
-            if len(tokens) <= tokenizer.model_max_length - 2  # [CLS] and [SEP]
-        ]
+) -> BatchEncoding:
+    # for word in data.keys():
+    logging.info("Tokenizing %s split...", split)
+    word_tokens = [
+        tokens
+        for word in data.keys()
+        for tokens in data[word].tokens(split)
+        if len(tokens) <= tokenizer.model_max_length - 2  # [CLS] and [SEP]
+    ]
 
-        if (diff := len(data[word].split(split)) - len(word_tokens)) > 0:
-            logging.warning(
-                "Removed %s entries for %s in %s, as they exceeded the maximum length of %s.",
-                diff,
-                word,
-                split,
-                tokenizer.model_max_length,
-            )
+    # if (diff := len(data[word].split(split)) - len(word_tokens)) > 0:
+    #     logging.warning(
+    #         "Removed %s entries for %s in %s, as they exceeded the maximum length of %s.",
+    #         diff,
+    #         word,
+    #         split,
+    #         tokenizer.model_max_length,
+    #     )
 
-        yield word, tokenizer(
-            word_tokens,
-            is_split_into_words=True,
-            padding=PaddingStrategy.MAX_LENGTH,
-            return_tensors=TensorType.PYTORCH,
-        )
+    return tokenizer(
+        word_tokens,
+        is_split_into_words=True,
+        padding=PaddingStrategy.MAX_LENGTH,  # Pad to max input length of model
+        return_tensors=TensorType.PYTORCH,
+        return_attention_mask=True,  # Return attention masks to distinguish padding from input
+    )
 
 
-def foo():
-    pass
+def vectorise(encoder: Encoder, model: Model):
+    for word, batch in encoder:
+        logging.info("Vectorising word '%s'...", word)
+        yield word, model(**batch)
