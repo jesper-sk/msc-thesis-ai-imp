34465bc74b8fb37ef2ffde12d89aba5fa5c267ef
jesper-sk <jesper.kuiper99@gmail.com>
2023-05-15 11:33:25 +0200

    Add batch helper

---
+++ wsd/util/helpers.py
@@ -0,0 +1,25 @@
+from typing import Iterable
+
+
+def batched(iterable: Iterable, batch_size: int) -> Iterable:
+    """Batch data from the `iterable` into tuples of length `batch_size`.
+    The last batch may be shorter than `batch_size`.
+
+    Parameters
+    ----------
+    `iterable : Iterable`
+        The iterable that is to be batched.
+
+    `batch_size : int`
+        The size of every batch. Must be higher than zero.
+
+    Returns
+    -------
+    `Iterable`
+        The batched iterable.
+    """
+    if n < 1:
+        raise ValueError("n must be at least one")
+    iterator = iter(iterable)
+    while batch := tuple(islice(iterator, batch_size)):
+        yield batch
