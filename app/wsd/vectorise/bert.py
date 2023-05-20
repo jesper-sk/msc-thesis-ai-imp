81a2608a6e65a97c73b0dae462f4800774491eda
jesper-sk <jesper.kuiper99@gmail.com>
2023-05-20 14:29:29 +0200

    Cleanup of bert

--- app/wsd/vectorise/bert.py
+++ app/wsd/vectorise/bert.py
@@ -31,16 +31,18 @@ class BertVectoriser:
         layers_of_interest: Optional[list[int]] = None,
         subword_merge_operation: SubwordMergeOperation = "first",
         layer_merge_operation: LayerMergeOperation = "sum",
+        device: str | None = None,
     ):
         self.model: Model
         self.tokenizer: Tokenizer
         self.model_name_or_path: str | PathLike[Any]
         self.layers_of_interest: list[int] = layers_of_interest or [-1]
+        self.device = device
 
-        self.merge_subword_tokens = self.get_subword_merge_function(
+        self.merge_subword_tokens = self._get_subword_merge_function(
             subword_merge_operation
         )
-        self.merge_layers = self.get_layer_merge_function(layer_merge_operation)
+        self.merge_layers = self._get_layer_merge_function(layer_merge_operation)
 
         if (model is None) ^ (tokenizer is None):
             raise ValueError(
@@ -57,12 +59,12 @@ class BertVectoriser:
         else:
             self.model_name_or_path = model_name_or_path
             if preload:
-                self.ensure_models_loaded()
+                self.load_prepare_models()
 
     def _is_loaded(self) -> bool:
         return hasattr(self, "model") and hasattr(self, "tokenizer")
 
-    def ensure_models_loaded(self) -> None:
+    def load_prepare_models(self) -> None:
         if not self._is_loaded():
             self.tokenizer = trans.BertTokenizerFast.from_pretrained(
                 self.model_name_or_path
@@ -74,50 +76,58 @@ class BertVectoriser:
                 "Tokenizer should be a 'fast' variant (e.g. BertTokenizerFast)"
             )
 
-    @classmethod
-    def get_subword_merge_function(
-        cls, operation: SubwordMergeOperation
+        self.model.eval()
+        if self.device:
+            self.model.to("cuda")
+
+    def to(self, device: str) -> None:
+        self.device = device
+
+    def _get_subword_merge_function(
+        self, operation: SubwordMergeOperation
     ) -> Callable[[Tensor], Tensor]:
         try:
-            return getattr(cls, f"merge_subword_{operation}")
+            return getattr(self, f"_merge_subword_{operation}")
         except AttributeError:
             raise ValueError(
-                f"Invalid subword merge operation: cannot find function `cls.merge_subword_{operation}`"
+                f"Invalid subword merge operation: cannot find function `self._merge_subword_{operation}`"
                 " corresponding to operation '{operation}'"
             )
 
-    @classmethod
-    def get_layer_merge_function(
-        cls, operation: LayerMergeOperation
-    ) -> Callable[[list[Tensor]], Tensor]:
-        try:
-            return getattr(cls, f"merge_layers_{operation}")
-        except AttributeError:
-            raise ValueError(
-                f"Invalid layer merge operation: cannot find function `cls.merge_layers_{operation}`"
-                " corresponding to operation '{operation}'"
-            )
-
-    @staticmethod
-    def merge_layers_sum(layers: list[Tensor]) -> Tensor:
-        return sum(layers, Tensor([0]))
-
-    @staticmethod
-    def merge_subword_first(layer: Tensor) -> Tensor:
+    def _merge_subword_first(self, layer: Tensor) -> Tensor:
         return layer[0, :]
 
-    @staticmethod
-    def merge_subword_mean(layer: Tensor) -> Tensor:
+    def _merge_subword_mean(self, layer: Tensor) -> Tensor:
         return layer.mean(dim=0)
 
+    def _get_layer_merge_function(
+        self, operation: LayerMergeOperation
+    ) -> Callable[[list[Tensor]], Tensor]:
+        try:
+            return getattr(self, f"_merge_layers_{operation}")
+        except AttributeError:
+            raise ValueError(
+                f"Invalid layer merge operation: cannot find function `self._merge_layers_{operation}`"
+                " corresponding to operation '{operation}'"
+            )
+
+    def _merge_layers_sum(self, layers: list[Tensor]) -> Tensor:
+        return sum(layers, self.tensor([0]))
+
+    def tensor(self, data, *, dtype=None):
+        return torch.tensor(data, dtype=dtype, device=self.device)
+
     def encode_tokens(self, tokens: Tokens | Sequence[Tokens]) -> TokenizerOutput:
-        return self.tokenizer(
+        output = self.tokenizer(
             tokens,
             is_split_into_words=True,
             padding=PaddingStrategy.LONGEST,
             return_tensors=TensorType.PYTORCH,
             return_attention_mask=True,  # Return attention masks to distinguish padding from input
         )
+        if self.device:
+            return output.to(self.device)
+        return output
 
     def model_pass(self, encoded: TokenizerOutput) -> ModelOutput:
         return self.model(**encoded, output_hidden_states=True)
@@ -149,14 +159,34 @@ class BertVectoriser:
             for entry_id in range(num_sentences)
         ]
 
+    def extract_target_embeddings(
+        self, merged_embeddings: Tensor, target_token_indices: list[tuple[int, int]]
+    ) -> Tensor:
+        ret = torch.zeros(
+            merged_embeddings.shape[0::2]
+        )  # shape: (batch_size, embedding_dim)
+
+        for sentence_index, (encoding, (start_index, end_index)) in enumerate(
+            zip(merged_embeddings, target_token_indices)
+        ):
+            subword_tokens = encoding[start_index:end_index, :]
+            ret[sentence_index, :] = self.merge_subword_tokens(subword_tokens)
+
+        return ret
+
+    def _iter_cleanup(self):
+        if self.device == "cuda":
+            torch.cuda.empty_cache()
+
     def __call__(self, data: list[Entry], batch_size: int = 1) -> Iterator[Tensor]:
-        self.ensure_models_loaded()
+        self.load_prepare_models()
 
         for batch in batched(data, batch_size):
             entries = transpose_entries(batch)
             encoded = self.encode_tokens(entries.tokens)
+
             target_token_indices = self.get_target_subword_token_ranges(
-                encoded.word_ids, entries.target_word_ids, batch_size
+                encoded.word_ids, entries.target_word_ids, len(encoded.encodings)
             )
             model_output = self.model_pass(encoded)
             merged_embeddings = self.merge_layers(
@@ -165,15 +195,9 @@ class BertVectoriser:
                     for layer_id in self.layers_of_interest
                 ]
             )
+            target_embeddings = self.extract_target_embeddings(
+                merged_embeddings, target_token_indices
+            )
 
-            output = torch.zeros(
-                merged_embeddings.shape[0::2]
-            )  # shape: (batch_size, embedding_dim)
-
-            for sentence_index, (encoding, (start_index, end_index)) in enumerate(
-                zip(merged_embeddings, target_token_indices)
-            ):
-                subword_tokens = encoding[start_index:end_index, :]
-                output[sentence_index, :] = self.merge_subword_tokens(subword_tokens)
-
-            yield output
+            self._iter_cleanup()
+            yield target_embeddings.detach()  # detaching from autograd graph, pevents OOM
